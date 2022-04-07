ac91d32f551d0232c56f46637741a31c1e0d24bd
borenoitg <daniel.anani@outlook.com>
2022-04-07 16:14:02 +0200

    Assignment Review Create Yanny JUnit 5 Laurel Test with Spring Context

---
+++ src/test/java/org/springframework/samples/petclinic/sfg/junit5/HearingInterpreterYannyTest.java
@@ -0,0 +1,29 @@
+package org.springframework.samples.petclinic.sfg.junit5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.sfg.BaseConfig;
+import org.springframework.samples.petclinic.sfg.HearingInterpreter;
+import org.springframework.samples.petclinic.sfg.YannyConfig;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author : eanani
+ * @project : tb2g-testing-spring
+ * @created : 07/04/2022,
+ **/
+@SpringJUnitConfig(classes = {BaseConfig.class, YannyConfig.class})
+class HearingInterpreterYannyTest {
+
+    @Autowired
+    HearingInterpreter hearingInterpreter;
+
+    @Test
+    void whatIheard() {
+        String word = hearingInterpreter.whatIheard();
+
+        assertEquals("Yanny", word);
+    }
+}
