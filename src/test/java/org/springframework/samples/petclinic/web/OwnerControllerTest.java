9d4082278eac73d8faa3d578bfeb40d73ce60a1e
borenoitg <daniel.anani@outlook.com>
2022-04-11 11:45:36 +0200

    Spring MVC : Spring Web Configuration with XML

---
+++ src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
@@ -0,0 +1,27 @@
+package org.springframework.samples.petclinic.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.service.ClinicService;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+
+/**
+ * @author : eanani
+ * @project : tb2g-testing-spring
+ * @created : 11/04/2022,
+ **/
+@SpringJUnitWebConfig(locations = {"classpath:spring/mvc-test-config.xml", "classpath:spring/mvc-core-config.xml"})
+class OwnerControllerTest {
+
+    @Autowired
+    OwnerController ownerController;
+
+    @Autowired
+    ClinicService clinicService;
+
+    @Test
+    void tempTest() {
+
+        // OwnerController
+    }
+}
